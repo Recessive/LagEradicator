ea4e0c458a860b0c8a8a2462065f2650c0de36bd
Recessive <AA.mindustry@gmail.com>
2022-02-02 06:43:48 +1100

    Changed lag eradicator to just kill monos instead

--- src/main/EradicatorMain.java
+++ src/main/EradicatorMain.java
@@ -37,77 +37,27 @@ import static mindustry.Vars.*;
 
 public class EradicatorMain extends Plugin {
 
-    private HashMap<String, Integer> teamMonos = new HashMap<>();
-
-    private final static int tenSecondTime = 10;
-    private RTInterval tenSecondInterval = new RTInterval(tenSecondTime);
-
-    private float realTime = 0f;
-    private int seconds = 0;
-
-    private static long startTime = System.currentTimeMillis();
+    private Seq<ItemStack> monoItems = ItemStack.list(
+            Items.copper, 300,
+                   Items.lead, 300);
 
     public void init(){
 
-        Seq<ItemStack> monoCost = ItemStack.list(Items.silicon, 30, Items.lead, 15);
-
         Events.on(EventType.UnitCreateEvent.class, event -> {
-            if(event.unit.type == UnitTypes.mono){
+            if(event.unit.type == UnitTypes.mono) {
+                event.unit.health = 0;
 
 
-                String teamName = event.unit.team().name;
-                teamMonos.putIfAbsent(teamName, 0);
-                int monoCount = teamMonos.get(teamName)+1;
-                int monoLimit = 0;
-                for(CoreBlock.CoreBuild core : event.unit.team.cores()){
-                    if(core.block == Blocks.coreShard){
-                        monoLimit += 8;
-                    }else if (core.block == Blocks.coreFoundation){
-                        monoLimit += 16;
-                    }else if (core.block == Blocks.coreNucleus){
-                        monoLimit += 24;
-                    }
+                // Let players know they got resources
+                Call.label("\uF838+" + monoItems.get(0) + "\n+" + monoItems.get(1) + "\uF837", 5, event.unit.x, event.unit.y);
+
+                Tile tile = event.unit.team.core().tile;
+
+                for (ItemStack stack : monoItems) {
+                    Call.transferItemTo(null, stack.item, stack.amount, tile.drawx(), tile.drawy(), tile.build);
                 }
-
-                if (monoCount > monoLimit){
-                    Tile tile = event.unit.team().core().tile;
-                    for(ItemStack stack : monoCost){
-                        Call.transferItemTo(null, stack.item, stack.amount, tile.drawx(), tile.drawy(), tile.build);
-                    }
-                }else {
-                    teamMonos.put(teamName, monoCount);
-                }
-
-                event.unit.kill();
-                event.unit.dead(true);
-
-
             }
         });
 
-        Events.on(EventType.Trigger.class, event -> {
-
-            if (tenSecondInterval.get(seconds)) {
-                for (Teams.TeamData team : state.teams.active){
-                    if(teamMonos.getOrDefault(team.team.name, 0) > 0){
-                        Tile tile = team.core().tile;
-
-                        Seq<ItemStack> monoItems = ItemStack.list(
-                                Items.copper, 20*teamMonos.getOrDefault(team.team.name, 0),
-                                Items.lead, 20*teamMonos.getOrDefault(team.team.name, 0));
-
-                        for(ItemStack stack : monoItems){
-                            Call.transferItemTo(null, stack.item, stack.amount, tile.drawx(), tile.drawy(), tile.build);
-                        }
-
-                        Call.label("+" + monoItems.get(0).amount + "\uF838 +" + monoItems.get(1).amount + "\uF837",
-                                1f, tile.drawx(), tile.drawy());
-                    }
-                }
-            }
-
-            realTime = System.currentTimeMillis() - startTime;
-            seconds = (int) (realTime / 1000);
-        });
     }
 }
